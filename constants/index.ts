8422f51c37dc8fe62b3dea8c39516e58d2e09730
janjao937 <janjao937@gmail.com>
2024-11-29 17:04:53 +0700

    finished base sidebar

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+const sidebarLinks=[
+    {
+        lable:"Home",
+        route:"/",
+        imgUrl:"/icon/Home.svg",
+    },
+    {
+        lable:"Upcoming",
+        route:"/upcoming",
+        imgUrl:"/icon/Upcomming.svg",
+    },
+    {
+        lable:"Previous",
+        route:"/previous",
+        imgUrl:"/icon/Previous.svg",
+    },
+    {
+        lable:"Recordings",
+        route:"/recordings",
+        imgUrl:"/icon/video.svg",
+    },
+    {
+        lable:"Personal Room",
+        route:"/personal-room",
+        imgUrl:"/icon/add-personal.svg",
+    },
+]
+
+export {sidebarLinks}
