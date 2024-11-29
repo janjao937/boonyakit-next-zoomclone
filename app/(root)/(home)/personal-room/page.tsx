2101d8a08097766000f62f1633ca8aad7f8d4297
janjao937 <janjao937@gmail.com>
2024-11-29 16:59:42 +0700

    fix page font style

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PersonalRoom() {
     return (
         <section className="flex size-full flex-col gap-10 text-white">
-            <h1 className='text-3xl font-blod'>PersonalRoom</h1>
+            <h1 className='text-3xl font-bold'>PersonalRoom</h1>
         </section>
     )
 }
